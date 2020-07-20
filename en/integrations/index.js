1e61b88fee7feb78f7ade7cd886a7d0785cdce8a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-07-20 17:27:48 +0200

    Add xiaomi devices

--- en/integrations/index.js
+++ en/integrations/index.js
@@ -1,7 +1,8 @@
 import philipsHue from "./philips-hue.json";
 import sonoff from "./sonoff.json";
 import zwave from "./zwave.json";
+import xiaomi from "./xiaomi.json";
 
-const integrations = philipsHue.concat(sonoff).concat(zwave);
+const integrations = philipsHue.concat(sonoff).concat(zwave).concat(xiaomi);
 
 export default integrations;
