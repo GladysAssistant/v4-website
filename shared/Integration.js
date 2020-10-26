92eb281234c738ff3df28fbe63104d4aec459b00
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-10-26 15:50:52 +0200

    Add filtering to the Integration page and improving responsive

--- shared/Integration.js
+++ shared/Integration.js
@@ -8,6 +8,8 @@ import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 const truncate = (str, len) =>
   str.substring(0, (str + " ").lastIndexOf(" ", len));
 
+const FILTERS = ["philips-hue", "zwave", "tasmota", "camera", "xiaomi"];
+
 function Integration({
   docsId,
   imageName,
@@ -79,21 +81,43 @@ function Integration({
 
 function IntegrationPage({ integrations, lang, translation }) {
   const [searchTerm, setSearchTerm] = React.useState("");
+  const [checkedTags, setCheckedTags] = React.useState({
+    "philips-hue": true,
+    zwave: true,
+    camera: true,
+    tasmota: true,
+    xiaomi: true,
+  });
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
+  const handleCheckedChange = (event) => {
+    const newCheckedTags = {
+      ...checkedTags,
+      [event.target.name]: event.target.checked,
+    };
+    setCheckedTags(newCheckedTags);
+  };
   const integrationsFiltered = integrations.filter((integration) => {
+    let validWithSearch = true;
+    let validWithTags = true;
+
+    // validating the query with the search params
     if (searchTerm && searchTerm.length > 2) {
-      return (
+      validWithSearch =
         integration.title.toLowerCase().search(searchTerm.toLowerCase()) !==
           -1 ||
         integration.description
           .toLowerCase()
-          .search(searchTerm.toLowerCase()) !== -1
-      );
-    } else {
-      return true;
+          .search(searchTerm.toLowerCase()) !== -1;
     }
+
+    // validating the query with the tags
+    if (checkedTags) {
+      validWithTags = checkedTags[integration.docsId];
+    }
+
+    return validWithSearch && validWithTags;
   });
 
   return (
@@ -102,14 +126,33 @@ function IntegrationPage({ integrations, lang, translation }) {
       style={{ paddingTop: "30px", paddingBottom: "50px" }}
     >
       <div className="row">
-        <div className="col col col--3">
-          <div class="navbar__search">
+        <div className={classnames("col col--3", styles.filtersSidebar)}>
+          <div className={classnames("navbar__search", styles.filterSearch)}>
             <input
-              class="navbar__search-input"
+              className={classnames(
+                "navbar__search-input",
+                styles.filterSearchInput
+              )}
               placeholder="Search"
               value={searchTerm}
               onChange={handleChange}
             />
+          </div>
+          <h3 className={classnames(styles.filterBlock, styles.filterTitle)}>
+            {translation.technologies}
+          </h3>
+          <div className={classnames(styles.filterBlock)}>
+            {FILTERS.map((filter) => (
+              <div className={classnames(styles.integrationFilterCheckbox)}>
+                <input
+                  type="checkbox"
+                  name={filter}
+                  checked={checkedTags[filter]}
+                  onChange={handleCheckedChange}
+                />
+                <label for={filter}>{translation.filters[filter]}</label>
+              </div>
+            ))}
           </div>
         </div>
         <div className="col col col--9">
