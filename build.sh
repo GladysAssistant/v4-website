311b6c00c4e1af7a7f3c2cd6c9822a0fe7a5e6eb
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-03-18 15:43:33 +0800

    Update build.sh script

--- build.sh
+++ build.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 set -e
 
-export NODE_OPTIONS='--max_old_space_size=4096' 
-
-rm -rf _build
-mkdir _build
+rm -rf build
 
 # Installing deps
-npm install
+yarn
 
-# Building en website
-cd en
+# Building website
 npm run build
-mv ./build ../_build/en
-cd ..
-
-# Building fr website
-cd fr
-npm run build
-node ../scripts/change-language.js
-mv ./build ../_build/fr
-cd ..
 
 # Moving netlify config
-cp ./netlify.toml ./_build/netlify.toml
+cp ./netlify.toml ./build/netlify.toml
 
 # Moving robots.txt
-cp ./robots.txt ./_build/robots.txt
+cp ./robots.txt ./build/robots.txt
 
 # Moving netlify functions
-cp -R ./functions ./_build/functions
+cp -R ./functions ./build/functions
