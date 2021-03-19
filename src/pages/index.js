173eac474d49744bf59d80c00ebd5e4b620031fb
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-03-19 16:34:01 +0800

    Add meta description/title to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,25 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import integrations from "../../integrations";
 import { Home } from "../components/Home";
 
+import { translate } from "@docusaurus/Translate";
+
 function HomePage() {
   const context = useDocusaurusContext();
   const { i18n } = context;
   const first4Integrations = integrations[i18n.currentLocale].slice(0, 4);
   return (
-    <Layout>
+    <Layout
+      title={translate({
+        id: "home.metaDescription",
+        description: "home page meta description",
+        message: "A privacy-first, open-source home assistant",
+      })}
+      description={translate({
+        id: "home.metaDescription",
+        description: "home page meta description",
+        message: "A privacy-first, open-source home assistant",
+      })}
+    >
       <Home integrations={first4Integrations} lang={i18n.currentLocale} />
     </Layout>
   );
