fcfd3b8447952e5ca1a52cdd5bfc2442e3e62be1
Vincent <47563262+tlse-vins@users.noreply.github.com>
2023-12-21 16:54:41 +0100

    docsintegrations 2

--- src/pages/docs-integrations.js
+++ src/pages/docs-integrations.js
@@ -24,7 +24,7 @@ function docsintegrations() {
       })}
      >
       <main>
-        <IntegrationPage
+        <Docs-integrationsPage
           integrations={integrations[i18n.currentLocale]}
           lang={i18n.currentLocale}
         />
