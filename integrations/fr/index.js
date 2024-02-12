ca681a762dce34dad825f45ccf0bb36febcc3509
Thomas LEMAISTRE <35010958+Terdious@users.noreply.github.com>
2024-02-12 14:39:50 +0100

    Update Airtable documentation + fix netatmo setup image size (#251)
    * fix: Update the size of Netatmo integration setup images that were too big
    
    * refactor: update airtable integrations
    
    * refactor: resizing images + formatting EN
    
    * refactor: new update devices integrations

--- integrations/fr/index.js
+++ integrations/fr/index.js
@@ -1,17 +1,23 @@
+import broadlink from "./broadlink.json";
+import camera from "./camera.json";
+import netatmo from "./netatmo.json";
 import philipsHue from "./philips-hue.json";
 import sonoff from "./sonoff.json";
-import zwave from "./zwave.json";
-import xiaomi from "./xiaomi.json";
-import camera from "./camera.json";
+import sonos from "./sonos.json";
 import tpLink from "./tp-link.json";
+import xiaomi from "./xiaomi.json";
 import zigbee2mqtt from "./zigbee2mqtt.json";
+import zwave from "./zwave.json";
 
 const integrations = zigbee2mqtt
-  .concat(sonoff)
-  .concat(xiaomi)
+  .concat(broadlink)
+  .concat(camera)
+  .concat(netatmo)
   .concat(philipsHue)
-  .concat(tpLink)
-  .concat(camera);
+  .concat(sonoff)
+  .concat(sonos)
+  .concat(xiaomi)
+  .concat(tpLink);
 // .concat(zwave);
 
 export default integrations;
